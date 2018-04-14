31ba2675ae274a0b1e24e06c20b24fa6029dc3fb
Jeff Guo <sguo35@gmail.com>
2018-04-13 22:48:12 -0700

    Only run 1 epoch

--- model.py
+++ model.py
@@ -149,5 +149,5 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbos
 
 history = model.fit([grades, subject_cat, subject_subcat, titles, essays_1, essays_2, resources, proj_price], y, validation_split=0.1,
                     verbose=1,
-          epochs=100, batch_size=32, callbacks=[reduce_lr])
+          epochs=1, batch_size=32, callbacks=[reduce_lr])
 model.save('./model.h5')
