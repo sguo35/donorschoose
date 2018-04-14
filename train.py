7b1b13696826102691fab4bfcde6eeca62225b65
Jeff Guo <sguo35@gmail.com>
2018-04-13 23:19:40 -0700

    Save model after each train run

--- train.py
+++ train.py
@@ -14,3 +14,5 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbos
 history = model.fit([grades, subject_cat, subject_subcat, titles, essays_1, essays_2, resources, proj_price], y, validation_split=0.1,
                     verbose=1,
           epochs=1, batch_size=32, callbacks=[reduce_lr])
+
+model.save('./model.h5')
