2529faa230563fa9dd770cee8223d8e239e31e04
Jeff Guo <sguo35@gmail.com>
2018-04-17 17:06:36 -0700

    Fix focal loss

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def focal_loss(gamma=2., alpha=.25):
 generator = DataGenerator(pandasFile=train_data, batch_size=32)
 valid_gen = DataGenerator(pandasFile=valid_data, batch_size=32)
 model.compile(optimizer=optimizers.sgd(nesterov=True, lr=0.01),
-			  loss=focal_loss,
+			  loss=focal_loss(),
 			  metrics=[auc_roc, 'acc'])
 model.fit_generator(generator=generator.gen_data(), use_multiprocessing=True, workers=4, epochs=5, steps_per_epoch=generator.__len__(), validation_data=valid_gen.gen_data(), validation_steps=valid_gen.__len__(), callbacks=[clr, checkpointer])
 
